a4ef60c272e8e494dd5c07db935d86d061237cea
lhmiao <2696254238@qq.com>
2018-10-31 10:57:53 +0800

    添加back,forward方法

--- hash_router/index.js
+++ hash_router/index.js
@@ -1,19 +1,31 @@
-let pre = document.querySelector('.pre');
-let next = document.querySelector('.next');
+let pre = document.querySelector('#pre');
+let next = document.querySelector('#next');
 let routerView = document.querySelector('.router-view');
 
 class Router {
   constructor () {
     this.routes = {};
     this.currentURL = '';
+    this.history = [];
+    this.currentIndex = this.history.length - 1;
+    this.isBackOrForward = false;
+    this.isReplace = false;
     this.reflesh = this.reflesh.bind(this);
+    this.back = this.back.bind(this);
+    this.forward = this.forward.bind(this);
     window.addEventListener('load', this.reflesh);
     window.addEventListener('hashchange', this.reflesh);
+    pre.addEventListener('click', this.back);
+    next.addEventListener('click', this.forward);
   }
 
   push (path) {
     location.hash = path;
-    this.reflesh();
+  }
+
+  replace (path) {
+    location.hash = path;
+    this.isReplace = true;
   }
 
   addRoute (path, callback) {
@@ -22,13 +34,38 @@ class Router {
 
   reflesh () {
     this.currentURL = location.hash.slice(1) || '/';
-    if (this.routes.hasOwnProperty(this.currentURL)) {
-      this.routes[this.currentURL]();
+    if (this.isReplace) {
+      this.history.splice(this.currentIndex, 1, this.currentURL);
+      this.isReplace = false;
+    } else if (this.isBackOrForward) {
+      this.isBackOrForward = false;
+    } else {
+      this.history.push(this.currentURL);
+      this.currentIndex = this.history.length - 1;
     }
+    this.routes[this.currentURL] && this.routes[this.currentURL]();
+  }
+
+  back () {
+    this.currentIndex && this.currentIndex--;
+    this.isBackOrForward = true;
+    location.hash = this.history[this.currentIndex] || '/';
+  }
+
+  forward () {
+    if (this.currentIndex < this.history.length - 1) {
+      this.currentIndex++;
+    }
+    this.isBackOrForward = true;
+    location.hash = this.history[this.currentIndex];
   }
 }
 
 let router = new Router();
+
+router.addRoute('/', () => {
+  routerView.innerHTML = '<p>这是首页</p>';
+});
 
 router.addRoute('/a', () => {
   routerView.innerHTML = '<p>这是页面A</p>';
